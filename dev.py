d8dce127936892b20afeb555899a52c58a9a752e
Ryan Wans <ryan.wan@icloud.com>
2023-05-28 14:34:52 -0400

    Automated Commit 2023-05-28T14:34:52-04:00

--- dev.py
+++ dev.py
@@ -1,10 +1,36 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 
 import images
 
-app = QtWidgets.QApplication(sys.argv)
+__state__ = {
+    "data_visible": True,
+    "gear": "PARK"
+}
 
-window = uic.loadUi("./v01pre.ui")
-window.show()
-app.exec()
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        # QtWidgets.QMainWindow.__init__(self, parent)
+        super(MainWindow, self).__init__(parent)
+        uic.loadUi("./v01pre.ui", self)
+        self.reset_but.clicked.connect(self.toggle_data_visibility)
+
+    @QtCore.pyqtSlot()
+    def toggle_data_visibility(self):
+        print("Toggling Data Group")
+        if(__state__["data_visible"]):
+            self.data.hide()
+            self.data_div.hide()
+        else:
+            self.data.show()
+            self.data_div.show()
+        __state__["data_visible"] = not __state__["data_visible"]
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+if __name__ == '__main__':
+	main()
