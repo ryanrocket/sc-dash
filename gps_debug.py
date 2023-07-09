552fb59c37ed873ac9c46bb765a78da5f5003d79
Ryan Wans <ryan.wan@icloud.com>
2023-07-08 21:36:49 -0400

    Automated Commit 2023-07-08T21:36:49-04:00

--- gps_debug.py
+++ gps_debug.py
@@ -1,1 +1,19 @@
 # debug gps over serial
+import time, serial, io, pynmea2
+
+ser = serial.Serial(
+    port = '/dev/ttyAMA0',
+    baudrate = 9600,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 1)
+
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+ser.reset_input_buffer()
+
+while True:
+    line = sio.readline()
+    msg = pynmea2.parse(line)
+    print(repr(msg))
+    time.sleep(0.5)
